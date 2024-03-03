3a71a6fe617e6e850808987ddb7e5378cfd17ab3
H31M44\Student <Student>
2024-03-03 14:40:10 +0700

    auto open file index

--- src/main/java/org/example/tesJdepend.java
+++ src/main/java/org/example/tesJdepend.java
@@ -2,45 +2,51 @@ package org.example;
 
 import jdepend.xmlui.JDepend;
 
+import java.awt.*;
 import java.io.*;
 import java.util.Scanner;
 
 public class tesJdepend {
     public static void main(String[] args) throws IOException, InterruptedException {
+        String projectUI = "T:\\PhamThanhSon\\jdepend-ui";
+        String pathReport = "T:\\son\\untitled\\rs.xml";
+        String scanProject = "T:\\Library-Assistant-master";
+
         try(PrintWriter printWriter = new PrintWriter(new FileOutputStream("rs.xml"))) {
             JDepend depend = new JDepend(printWriter);
-            depend.addDirectory("T:\\Library-Assistant-master");
+            depend.addDirectory(scanProject);
             depend.analyze();
             System.out.println("done");
-            String[] command = {"C:\\Program Files\\nodejs\\npm.cmd", "run", "T:\\PhamThanhSon\\jdepend-ui", "T:\\son\\untitled\\rs.xml", "xml"};
-
-            // Execute command
-            String npmCommand = "npm run jdepend-ui \"T:\\son\\untitled\\rs.xml\" \"xml\"";
-            String workingDirectory = "T:\\PhamThanhSon\\jdepend-ui";
-
-            try {
-                ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", npmCommand);
-                processBuilder.directory(new File(workingDirectory));
-                processBuilder.redirectErrorStream(true); // Redirect error stream to output stream
-                Process process = processBuilder.start();
-
-                // Read output
-                InputStream is = process.getInputStream();
-                Scanner scanner = new Scanner(is, "UTF-8");
-                while (scanner.hasNextLine()) {
-                    System.out.println(scanner.nextLine());
-                }
-
-                // Wait for the process to finish
-                int exitCode = process.waitFor();
-                System.out.println("Command exited with code " + exitCode);
-
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
+            runCommand(pathReport,projectUI);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
     }
+    public static void runCommand(String pathReport, String projectUI){
+        String npmCommand = "npm run jdepend-ui \""+pathReport+"\" \"xml\"";
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", npmCommand);
+            processBuilder.directory(new File(projectUI));
+            processBuilder.redirectErrorStream(true); // Redirect error stream to output stream
+            Process process = processBuilder.start();
+
+            // Read output
+            InputStream is = process.getInputStream();
+            Scanner scanner = new Scanner(is, "UTF-8");
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+
+            // Wait for the process to finish
+            int exitCode = process.waitFor();
+            System.out.println("Command exited with code " + exitCode);
+            File file = new File(projectUI+"\\index.html");
+            if (file.exists()){
+                Desktop.getDesktop().open(file);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
